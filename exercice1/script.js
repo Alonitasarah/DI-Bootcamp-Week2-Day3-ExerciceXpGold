12c7b7888112db99a66195c34fcefb94f6a3a3eb
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-11 11:12:59 +0000

    Add files via upload

---
+++ exercice1/script.js
@@ -0,0 +1,9 @@
+let numero = [123, 8409, 100053, 333333333, 7];
+
+for (let element of numero) {
+  if (element % 3 == 0) {
+    console.log(element + " est divisible par trois");
+  } else {
+    console.log(element + " est divisible par trois");
+  }
+}
